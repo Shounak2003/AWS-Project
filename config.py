d64d4865a411cea0497c6dcc50443376bd50e02a
Shounak Chandra <93007487+Shounak2003@users.noreply.github.com>
2023-10-20 03:02:12 +0530

    Create config.py
    Added Configuration file

---
+++ config.py
@@ -0,0 +1,23 @@
+# Configuration Parameters for Database and AWS S3 Storage
+
+# Database Configuration
+customhost = "http://employee.csrgslmtfzz7.ap-south-1.rds.amazonaws.com"
+customuser = "awsproject"
+custompass = "awsproject123"
+customdb = "employee"
+
+# AWS S3 Storage Configuration
+custombucket = "addemployee"
+customregion = "bombay"
+
+# The code defines configuration parameters for connecting to an AWS RDS database and using an AWS S3 bucket for storage.
+
+# Database Configuration:
+# - customhost: The endpoint URL of the AWS RDS database.
+# - customuser: The username for database access.
+# - custompass: The password for database access.
+# - customdb: The name of the database to connect to.
+
+# AWS S3 Storage Configuration:
+# - custombucket: The name of the AWS S3 bucket for storing files, in this case, "addemployee."
+# - customregion: The AWS region where the S3 bucket is located, in this case, "bombay."
